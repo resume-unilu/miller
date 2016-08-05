912bb9f8b12c9c1a4a345b6bbfcba097930f8fea
Daniele Guido <gui.daniele@gmail.com>
2016-08-05 17:33:11 +0200

    change default permission for rest framework

--- miller/settings.py
+++ miller/settings.py
@@ -140,9 +140,9 @@ USE_TZ = True
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
     'PAGE_SIZE': 10
 }
 
