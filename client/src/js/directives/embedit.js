ad1d7a8446644918a025223ac72faf5f844992c3
Daniele Guido <gui.daniele@gmail.com>
2016-10-14 11:14:32 +0200

    refactoring embedit function
    clean metadata templates

--- client/src/js/directives/embedit.js
+++ client/src/js/directives/embedit.js
@@ -7,7 +7,7 @@
  * If an object and a language are provided, it handles the translation.
  */
 angular.module('miller')
-  .directive('embedit', function($sce){
+  .directive('embedit', function($sce) {
     return {
       restrict : 'A',
       scope:{
@@ -16,32 +16,31 @@ angular.module('miller')
         language: '=',
         firstline: '='
       },
-      link: function(scope, element, attrs){
+      link: function(scope, element, attrs) {
         // console.log('::embedit @link, language:', scope.language, scope.embedit)
-        
-        if(scope.language && typeof scope.embedit == 'object'){
-          var altlanguage = scope.language.replace(/_[A-Z][A-Z]$/, '');
-          
-          if(scope.firstline){
-            // debugger
-            element.html((scope.embedit[scope.language]||scope.embedit[altlanguage]||'').split(/<br\s?\/?>/).shift());
-          } else {
-            element.html(scope.embedit[scope.language]||scope.embedit[altlanguage]||'');
-          }
-        } else {
-          element.html(scope.embedit);
-        }
         if(scope.stretch){
           element.find('iframe').width('100%').height('100%');
         }
 
+        scope.render = function(language) {
+          if(language && typeof scope.embedit == 'object') {
+            var altlanguage = scope.language.replace(/_[A-Z][A-Z]$/, '');
+            if(scope.firstline)
+              element.html((scope.embedit[language]||scope.embedit[altlanguage]||'').split(/<br\s?\/?>/).shift());
+            else
+              element.html(scope.embedit[language]||scope.embedit[altlanguage]||'');
+            
+          } else {
+            element.html(scope.embedit);
+          }
+        };
+        
+
         // enable listeners
-        if(scope.language && typeof scope.embedit == 'object'){
-          scope.$watch('language', function(language){
-            if(typeof scope.embedit == 'object'){
-              element.html(scope.embedit[scope.language]|| '');
-            }
-          })
+        if(scope.language && typeof scope.embedit == 'object') {
+          scope.$watch('language', scope.render);
+        } else{
+          scope.render();
         }
       }
     }
