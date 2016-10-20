e294d8cb9e992815f4f6ef3f260119dfebac3a81
Daniele Guido <gui.daniele@gmail.com>
2016-10-20 17:59:45 +0200

    rich oembed

--- client/src/js/directives/rich-oembed.js
+++ client/src/js/directives/rich-oembed.js
@@ -6,20 +6,30 @@
  * Rich oembed directive, with autoplay. Includes embedit directive as well.
  */
 angular.module('miller')
-  .directive('richOembed', function($sce, $log, RUNTIME) {
+  .directive('richOembed', function($sce, $log, $timeout, RUNTIME) {
     return {
       restrict : 'A',
       scope:{
-        autoplay: '=',
+        enabled: '=',
         oembed: '=',
         cover: '='
       },
       templateUrl: RUNTIME.static + 'templates/partials/directives/rich-oembed.html',
       
       link: function(scope, element, attrs) {
-        scope.enabled = false;
-        
+        // scope.enabled = false;
+        var timer;
+
         $log.log('🍩 rich-oembed ready', scope.cover);
+        scope.$watch('enabled', function(v){
+          $log.debug('🍩 rich-oembed @enabled:', v);
+          if(timer)
+            $timeout.cancel(timer);
+          timer = $timeout(function(){
+            $log.log('🍩 rich-oembed apply iframeEnabled:', v);
+            scope.iframeEnabled = v
+          }, 500);
+        });
       }
     }
   });
