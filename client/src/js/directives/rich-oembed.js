991db51173704fb6f798d95bf6a6764b13fe525e
Daniele Guido <gui.daniele@gmail.com>
2016-10-25 09:23:53 +0200

    cleaner side documents

--- client/src/js/directives/rich-oembed.js
+++ client/src/js/directives/rich-oembed.js
@@ -20,16 +20,26 @@ angular.module('miller')
         // scope.enabled = false;
         var timer;
 
+        scope.iframeEnabled = false;
+
+
         $log.log('🍩 rich-oembed ready', scope.cover);
         scope.$watch('enabled', function(v){
           $log.debug('🍩 rich-oembed @enabled:', v);
+          scope.toggleEnable(!!v);
+        });
+
+        scope.toggleEnable = function(enabled){
+          $log.log('🍩 rich-oembed > toggleEnable()', enabled);
+          var v = enabled === undefined? !scope.iframeEnabled:  enabled;
           if(timer)
             $timeout.cancel(timer);
           timer = $timeout(function(){
+
             $log.log('🍩 rich-oembed apply iframeEnabled:', v);
             scope.iframeEnabled = v
           }, 500);
-        });
+        }
       }
     }
   });
