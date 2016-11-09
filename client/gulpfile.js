d807d2b1108f7d06452bbd1f4ce0db81db600661
Daniele Guido <gui.daniele@gmail.com>
2016-11-09 10:28:27 +0100

    add missing module (socialshare)

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -52,6 +52,7 @@ gulp.task('libs', function() {
     './src/js/lib/angular-disqus.min.js',
     './src/js/lib/angular-debounce.js', 
     // './src/js/lib/angular-animate.min.js',
+    './src/js/lib/angular-socialshare.min.js',
     './src/js/lib/angular-ui-router.min.js', 
     './src/js/lib/angular-strap.min.js',
     './src/js/lib/angular-strap.tpl.min.js',
