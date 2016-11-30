71a7d0610bebb587040869f36b5f55ad9cca7eae
Daniele Guido <gui.daniele@gmail.com>
2016-11-30 10:17:56 +0100

    fix previous commit
    exception: AttributeError at /'AnonymousUser' object has no attribute
    'profile’. Profile need to be checked before

--- miller/context_processors.py
+++ miller/context_processors.py
@@ -4,11 +4,15 @@ from miller.api.serializers import HeavyProfileSerializer
 
 # inject some miller specific settings used by angular
 def default(request):
+  profile = HeavyProfileSerializer(request.user.profile).data if hasattr(request.user, 'profile') else {
+    'authors': []
+  }
+
   context_settings = {
     'title': settings.MILLER_TITLE,
     'debug': settings.MILLER_DEBUG,
     'settings': json.dumps(settings.MILLER_SETTINGS),
     'oembeds': json.dumps(settings.MILLER_OEMBEDS),
-    'profile': json.dumps(HeavyProfileSerializer(request.user.profile).data)
+    'profile': json.dumps( profile )
   }
   return context_settings
