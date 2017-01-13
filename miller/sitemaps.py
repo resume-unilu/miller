6981281a177e23d638f395fe07b3656913695768
Daniele Guido <gui.daniele@gmail.com>
2017-01-13 10:23:16 +0100

    improve sitemap, story appears with its public tags

--- miller/sitemaps.py
+++ miller/sitemaps.py
@@ -8,7 +8,7 @@ class NewsSitemap(Sitemap):
   priority = 0.5
 
   def items(self):
-    return Story.objects.filter(tags__category='blog', status=Story.PUBLIC).distinct()
+    return Story.objects.filter(tags__category='blog', status=Story.PUBLIC).distinct().orderby('-date_last_modified').prefetch_related('tags')
 
   def lastmod(self, obj):
     return obj.date_last_modified
@@ -20,7 +20,7 @@ class WritingsSitemap(Sitemap):
   priority = 0.5
 
   def items(self):
-    return Story.objects.filter(tags__category='writing', status=Story.PUBLIC).distinct()
+    return Story.objects.filter(tags__category='writing', status=Story.PUBLIC).distinct().order_by('-date').prefetch_related('tags')
 
   def lastmod(self, obj):
     return obj.date_last_modified
